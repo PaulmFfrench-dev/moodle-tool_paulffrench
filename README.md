db60cf79ce003391906d431af33871048d6f82bd
Paul Ffrench <paul@brickfieldlabs.ie>
2020-08-12 11:45:31 +0100

    Add version.php and travis ci file

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# moodle-tool_paulffrench
+# moodle-tool_paulmffrench-dev
+# 11/08/2020 - Plugin has version.php
+
+# 12/08/2020 - Added icon.png,icon.svg in /pix/ 
+# 12/08/2020 - $string['pluginname'] = 'My first Moodle plugin'; added in lang/en/paulmffrench-dev.php
---
+++ version.php
@@ -0,0 +1,17 @@
+<?php 
+defined('MOODLE_INTERNAL') || die();
+ 
+$plugin->component = 'tool_paulmffrench';
+$plugin->version = 202008111.0;
+// $plugin->requires = 2019111804.05;
+// $plugin->supported = [38, 39];   // Available as of Moodle 3.9.0 or later.
+// $plugin->incompatible = 39;   // Available as of Moodle 3.9.0 or later.
+// $plugin->component = 'tool_paulmffrench-dev';
+// $plugin->maturity = MATURITY_ALPHA;
+// $plugin->release = '3.8-r1';
+ 
+// $plugin->dependencies = [
+//     'mod_forum' => ANY_VERSION,
+//     'mod_data' => TODO
+// ];
+?>
