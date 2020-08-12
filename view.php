3494698921b74d9875f2ba87f4616964ee1b2adf
Paul Ffrench <paul@brickfieldlabs.ie>
2020-08-12 17:04:59 +0100

    Add index.php, view.php and  additional string to lang file

---
+++ view.php
@@ -0,0 +1,10 @@
+<?php 
+require_once(__DIR__ . '/../../../config.php');
+$url = new moodle_url('/admin/tool/paulmffrench/index.php');
+$PAGE->set_context(context_system::instance());
+$PAGE->set_url($url);
+$PAGE->set_pagelayout('report');
+$PAGE->set_title('Hello to the paulmffrench plugin');
+$PAGE->set_heading(get_string('pluginname', 'tool_paulmffrench'));
+
+?>
